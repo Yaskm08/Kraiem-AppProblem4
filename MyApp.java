205c8c8dbc51db242b8fea83ab922228865adf8a
Yaskm08 <148379527+Yaskm08@users.noreply.github.com>
2023-11-05 00:24:11 -0400

    Create MyApp.java

---
+++ MyApp.java
@@ -0,0 +1,12 @@
+public class MyApp {
+  public static void main (string[] args) {
+    System.out.println("Welcome to my App");
+    System.out.println("This is a simple Java ");
+    System.out.println("Feel free to make changes and improvements!");
+
+        int num1 = 5;
+        int num2 = 7;
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
